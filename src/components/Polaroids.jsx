030a9d2037367d40ed2924a84f897ebcd16ccb5f
Aung Pyae Sone <ww.aps2002@gmail.com>
2024-08-19 06:42:33 -0400

    Add motion

--- src/components/Polaroids.jsx
+++ src/components/Polaroids.jsx
@@ -1,23 +1,66 @@
 import polaroid from "../assets/image.jpg";
 import { POLAROIDS } from "../constant";
+import { motion } from "framer-motion";
+
+const containerVariants = {
+  hidden: { opacity: 0, y: 20 },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 1,
+      staggerChildren: 0.7,
+    },
+  },
+};
+
+const itemVariants = {
+  hidden: { opacity: 0, x: -50 },
+  visible: { opacity: 1, x: 0, transition: { duration: 1 } },
+};
+
 const Polaroids = () => {
   return (
     <div className="container mx-auto" id="polaroids">
-      <h2 className="mb-12 mt-20 text-center text-4xl font-semibold">
+      <motion.h2 className="mb-12 mt-20 text-center text-4xl font-semibold" 
+      initial={{ opacity: 0, y: 20 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      transition={{ duration: 1.5 }}
+      viewport={{ once: true }}>
         Polaroids
-      </h2>
-      <div className="mx-2 flex flex-col lg:flex-row rounded-xl bg-gradient-to-b from-zinc-900 to-zince-950 px-4 py-10 lg:px-20">
-        <div className="flex items-center lg:basis-1/2">
-          <img src={polaroid} alt="Nino's polaroid" className="w-50% lg:w-auto" />
-        </div>
-        <div className="text-md flex flex-col justify-center items-center lg:basis-1/2 space-y-6 p-9 ">
-        {Object.entries(POLAROIDS).map(([key, value], index) => (
-          <span key={index} className="py-1 border-b-2 border-yellow-200 font-semibold lg:text-xl">
-            <strong>{key}:</strong> {value}
-          </span>
-        ))}
-        </div>
-      </div>
+      </motion.h2>
+      <motion.div
+        className="mx-2 flex flex-col lg:flex-row rounded-xl bg-gradient-to-b 
+      from-zinc-900 to-zince-950 px-4 py-10 lg:px-20"
+        initial="hidden"
+        whileInView="visible"
+        variants={containerVariants}
+        viewport={{ once: true }}
+      >
+        <motion.div
+          className="flex items-center lg:basis-1/2"
+          variants={itemVariants}
+        >
+          <img
+            src={polaroid}
+            alt="Nino's polaroid"
+            className="lg:w-auto"
+          />
+        </motion.div>
+        <motion.div
+          className="text-md flex flex-col justify-center items-center lg:basis-1/2 space-y-6 p-9"
+          variants={itemVariants}
+        >
+          {Object.entries(POLAROIDS).map(([key, value], index) => (
+            <span
+              key={index}
+              className="py-1 border-b-2 border-yellow-200 font-semibold lg:text-xl"
+            >
+              <strong>{key}:</strong> {value}
+            </span>
+          ))}
+        </motion.div>
+      </motion.div>
     </div>
   );
 };
