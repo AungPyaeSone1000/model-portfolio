ba0ca39f0c9a9d56c7f39440f6b3680a2d057fc8
Aung Pyae Sone <ww.aps2002@gmail.com>
2024-08-19 06:28:29 -0400

    Add motion

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,24 +1,57 @@
 import { PROJECTS } from "../constant";
 import Card from "../components/Card";
+import { motion } from "framer-motion";
+
+const containerVariants = {
+  hidden: { opacity: 0, y: -20 },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: { duration: 1, staggerChildren: 0.7 },
+  },
+};
+
+const itemVariants = {
+  hidden: { opacity: 0 },
+  visible: {
+    opacity: 1,
+    scale: 1,
+    transition: { duration: 1 },
+  },
+};
 
 const Project = () => {
   return (
     <div id="projects">
-      <h2 className="mt-20 text-center text-4xl font-semibold ">Projects</h2>
-      <div className="flex flex-wrap justify-center py-8">
+      <motion.h2
+        className="mt-20 text-center text-4xl font-semibold"
+        initial="hidden"
+        whileInView="visible"
+        variants={containerVariants}
+        viewport={{ once: true }}
+      >
+        Projects
+      </motion.h2>
+      <motion.div
+        className="flex flex-wrap justify-center py-8"
+        initial="hidden"
+        whileInView="visible"
+        variants={containerVariants}
+        viewport={{ once: true }}
+      >
         {PROJECTS.map((project, index) => {
-          return ( 
-            <div key={index}>
+          return (
+            <motion.div key={index} variants={itemVariants}>
               <Card
                 image={project.image}
                 title={project.title}
                 subtitle={project.subtitle}
                 link="#"
               ></Card>
-            </div>
+            </motion.div>
           );
         })}
-      </div>
+      </motion.div>
     </div>
   );
 };
